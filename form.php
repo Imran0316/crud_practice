b856a90000433556c297b6531c940ea4259ba0ff
Imran0316 <ik775239@gmail.com>
2024-11-19 07:55:09 -0800

    add all files

---
+++ form.php
@@ -0,0 +1,24 @@
+<form action="">
+    <label for="name"> Name:</label> <br>
+    <input type="text" name="name"> <br>
+
+    <label for="email"> Email:</label> <br>
+    <input type="email" name="email"> <br>
+
+    <label for="password"> Password:</label> <br>
+    <input type="password" name="password"> <br>
+
+    <label for="phone"> Phone:</label> <br>
+    <input type="number" name="phone"> <br>
+
+    <label for="feed"> Feedback:</label> <br>
+    <textarea name="feed" cols="30" rows="10"></textarea>
+
+    <input type="submit" name="submit">
+</form>
+
+<?php
+
+
+
+?>
