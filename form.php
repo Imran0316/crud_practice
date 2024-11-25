abd6330e5c225bd0967e5bcfe9221727e0383a31
Imran0316 <ik775239@gmail.com>
2024-11-25 02:47:27 -0800

    add all files

--- form.php
+++ form.php
@@ -1,24 +1,39 @@
-<form action="">
-    <label for="name"> Name:</label> <br>
-    <input type="text" name="name"> <br>
 
-    <label for="email"> Email:</label> <br>
-    <input type="email" name="email"> <br>
+  <form action="" method="POST">
+    <input type="text" name="name" placeholder="NAME" > <br><br>
+    <input type="email" name="email" placeholder="EMAIL" > <br><br>
+    <input type="tel" name="phone" placeholder="PHONE" > <br><br>
+    <input type="number" name="age" placeholder="AGE" > <br><br>
+    <input type="text" name="address" placeholder="ADDRESS" > <br><br>
 
-    <label for="password"> Password:</label> <br>
-    <input type="password" name="password"> <br>
+    <input type="submit" value="submit" name="sub">
+  </form>
 
-    <label for="phone"> Phone:</label> <br>
-    <input type="number" name="phone"> <br>
-
-    <label for="feed"> Feedback:</label> <br>
-    <textarea name="feed" cols="30" rows="10"></textarea>
-
-    <input type="submit" name="submit">
-</form>
 
 <?php
+  if(isset($_POST["sub"])){
+    $pname=$_POST["name"];
+    $pemail=$_POST["email"];
+    $pphone=$_POST["phone"];
+    $page=$_POST["age"];
+    $padd=$_POST["address"];
 
+    $connection =mysqli_connect("localhost","root","","patients");
+    if($connection){
+      echo "working";
+    }else{
+      echo "not working";
+    }
 
+    $quer= "INSERT INTO `pt_details`( `patientname`, `email`, `phone`, `age`, `adress`) VALUES ('$pname','$pemail','$pphone','$page','$padd')";
 
+    $run=mysqli_query($connection,$quer);
+
+    if($run){
+      echo "connect";
+    }else{
+      echo "not connected";
+    }
+  }
 ?>
+
